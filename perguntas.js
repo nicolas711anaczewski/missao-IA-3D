bf838bdf439960fb8009ec3d6833e265056103d3
nicolas711anaczewski <nicolas.anaczewski@escola.pr.gov.br>
2024-09-23 09:30:45 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+const perguntas = [
+    {
+        enunciado: "oque voce acha sobre pessoas que fazem racismo",
+        alternativas: [
+            {
+                texto: "eu acho pessoas que fazem racismo mal carater",
+                afirmacao: [
+                    "eu acho pessoas que fazem racismo mal carater ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "voce apoia o racismo ",
+                afirmacao: [
+                    "enfraquecer a altoestima,perpetuando o sentimento de inferioridade",
+                    "nao, acho que o racismo ficou no passado"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
